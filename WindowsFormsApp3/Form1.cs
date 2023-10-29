a943bb423b7581b3b71fba7494942fcb3df50f2e
User <User@WIN-8L3JPGP323N>
2023-10-29 22:01:08 +0600

    Добавьте файлы проекта.

---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double i = Convert.ToDouble(textBox1.Text);
+            double b = Convert.ToDouble(textBox2.Text);
+            double c = Convert.ToDouble(textBox3.Text);
+            double result = (i + b) / c;
+            textBox4.Text = Convert.ToString(result);
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            {// check which radiobutton was selected
+                if (radioButton1.Checked == true)
+                    textBox5.Text = Convert.ToString(Convert.ToDouble(textBox4.Text) * 10 / 100);
+                if (radioButton2.Checked == true)
+                    textBox5.Text = Convert.ToString(Convert.ToDouble(textBox4.Text) * 20 / 100);
+                if (radioButton3.Checked == true)
+                    textBox5.Text = Convert.ToString(Convert.ToDouble(textBox4.Text) * 50 / 100);
+
+            }
+        }
+    }
+}
